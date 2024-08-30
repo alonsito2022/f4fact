a630a9bc15529685286fe075246d21e1126a222f
witalo <vent.wivf@gmail.com>
2024-08-30 13:22:20 -0500

    cambios

--- src/app/dashboard/users/users/UserList.tsx
+++ src/app/dashboard/users/users/UserList.tsx
@@ -40,7 +40,7 @@ function UserList({ users, modal, setModal, user, setUser }: any) {
                 <thead className="bg-gray-100 dark:bg-gray-700">
                     <tr>
                         <th scope="col" className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                            Nº
+                            Usuario
                         </th>
                         <th scope="col" className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
                             Documento
@@ -55,41 +55,39 @@ function UserList({ users, modal, setModal, user, setUser }: any) {
                             Telefono
                         </th>
                         <th scope="col" className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                            Correo
+                            Rol usuario
                         </th>
                         <th scope="col" className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                            Rol
-                        </th>
-                        <th scope="col" className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                            <span className="sr-only">Edit</span>
+                           Editar
                         </th>
                     </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                     {users.map((item: IUser) =>
                         <tr key={item.id} className="hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <th scope="row" className="w-4 p-4">
-                                {item.id}
-                            </th>
-                            <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td className="flex items-center p-2 mr-5 space-x-4 whitespace-nowrap">
+                                <img className="w-10 h-10 rounded-full" src="/images/users/{{ .avatar }}" alt="{{ .name }} avatar"/>
+                                <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                                    <div className="text-base font-semibold text-gray-900 dark:text-white">{item.firstName} {item.lastName}</div>
+                                    <div className="text-sm font-normal text-gray-500 dark:text-gray-400">{item.email}</div>
+                                </div>
+                            </td>
+                            <td className="p-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {item.document}
                             </td>
-                            <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td className="p-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {item.firstName}
                             </td>
-                            <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td className="p-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {item.lastName}
                             </td>
-                            <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td className="p-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {item.phone}
                             </td>
-                            <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {item.email}
-                            </td>
-                            <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td className="p-3 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {item.roleName}
                             </td>
-                            <td className="p-4 text-2xl font-medium text-gray-900 whitespace-nowrap dark:text-white text-right">
+                            <td className="p-3 text-2xl font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
                                 <a href="#" className="hover:underline" onClick={async () => {
                                     await fetchUserByID(item.id!);
                                     modal.show();
