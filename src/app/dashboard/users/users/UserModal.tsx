5946124c75577caa8d4cfa4f0cf90eeffca5e592
witalo <vent.wivf@gmail.com>
2024-08-30 17:22:51 -0500

    cambios

--- src/app/dashboard/users/users/UserModal.tsx
+++ src/app/dashboard/users/users/UserModal.tsx
@@ -191,14 +191,14 @@ function UserModal({modal, setModal, user, setUser, initialState, fetchUsers}:an
                                 src={ user.id&&user.avatar?.search("base64")==-1?`${process.env.NEXT_PUBLIC_BASE_API}/${user.avatar}`:user.avatar}
                                 alt="Imagen seleccionada"
                                 style={{ maxWidth: '100%', maxHeight: '200px' }}
-                              />:
-                              <>
+                            />:
+                            <>
                                 <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                                 </svg>
                                 <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Haga clic para cargar</span> o arrastrar y soltar</p>
                                 <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                              </> } 
+                            </> }  
                         </div>
                         <input id="avatar" name="avatar" type="file" className="hidden" ref={fileInputRef} onClick={handleFileReset} onChange={handleFileChange} accept="image/*"/>
                     </label>
