f4644f660c68bfbfe5c3fc1e1531a6d240d79fcc
witalo <vent.wivf@gmail.com>
2024-08-31 13:58:15 -0500

    cambiojs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -308,7 +308,6 @@ function Navbar() {
                                     <>
                                     <UserCircle/>                 
                                     </> */}
-                                }
                                 </button>
                             </div>
                            
