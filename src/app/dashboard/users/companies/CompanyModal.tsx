d4c2c21e188309e219704e5d01059922e13bfae3
witalo <vent.wivf@gmail.com>
2025-02-17 17:31:55 -0500

    cambios

--- src/app/dashboard/users/companies/CompanyModal.tsx
+++ src/app/dashboard/users/companies/CompanyModal.tsx
@@ -337,8 +337,8 @@ function CompanyModal({ modal, setModal, jwtToken, company, setCompany, initialS
                 if (e.target?.result) {
                     const img = new Image();
                     img.onload = () => {
-                        if (img.width > 320 || img.height > 80) {
-                            toast.error("La imagen no debe superar los 320x80 píxeles", {
+                        if (img.width > 300 || img.height > 300) {
+                            toast.error("La imagen no debe superar los 300x300 píxeles", {
                                 hideProgressBar: true,
                                 autoClose: 2000,
                             });
@@ -453,7 +453,7 @@ function CompanyModal({ modal, setModal, jwtToken, company, setCompany, initialS
                                                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                                                                 </svg>
                                                                 <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Haga clic para cargar</span> o arrastrar</p>
-                                                                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 400x400px)</p>
+                                                                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 300x300px)</p>
                                                             </>}
                                                     </div>
                                                     <input id="logo" name="logo" type="file" className="hidden" ref={fileInputRef} onClick={handleFileReset} onChange={handleFileChange} accept="image/*" />
